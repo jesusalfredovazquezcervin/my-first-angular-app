92f84b491165681789ac0de02d751430ef728044
JESUS ALFREDO VAZQUEZ CERVIN <jesus.alfredo.vazquez.cervin@gmail.com>
2022-09-27 16:34:08 -0500

    se agrega el codigo para el ejercicio 2

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -6,10 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  userName: string = ''; 
+  enabledButton = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  
+  onCreatedUser(){
+    return this.userName; 
+  }
+  onUpdatedUserName(event: Event){
+    this.userName = (<HTMLInputElement>event.target).value;
+    //"hello world".replace(/\s/g, "");
+
+    if(this.userName.replace(/\s/g, "")!= ""){
+      setTimeout(() =>{
+        this.enabledButton = true;
+      }, 100);
+    }
+    else{
+      setTimeout(() =>{
+        this.enabledButton = false;
+      }, 100);
+    }
+
+  }
+  cleanUserName(){
+    this.userName='';
+    setTimeout(() =>{
+      this.enabledButton = false;
+    }, 100);
+  }
+  
+
 
 }
