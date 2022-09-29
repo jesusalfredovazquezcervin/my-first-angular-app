ed84f7fe714dfa50407308705ebfd2ffd4ccf6ee
JESUS ALFREDO VAZQUEZ CERVIN <jesus.alfredo.vazquez.cervin@gmail.com>
2022-09-29 12:10:16 -0500

    se modifican los archivos porque los habia escrito mal, tenia codigo del SERVER dentro del SERVERS

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,33 +1,29 @@
+import { style } from '@angular/animations';
 import  { Component } from '@angular/core';
 @Component({
     selector: 'app-server',
-    templateUrl: './server.component.html'
+    templateUrl: './server.component.html',
+    styles: [
+        ` .online {
+            color: white;
+        }
+        `]
 })
 export class ServerComponent {
     serverID: number  = 10;
     serverStatus: string  = "Online";
-    allowNewServer = false;
-    serverCreationStatus = "No server created yet..."
-    serverName = '';
-    
-    
-    constructor(){
-        setTimeout(() =>{
-            this.allowNewServer = true;
-        }, 3000);
+  
+    constructor() {
+        this.serverStatus = Math.random() > 0.5 ? 'Online':'Offline';
     }
-
     getServerStatus() {
         return this.serverStatus;
 
-    } 
-
-    onCreatedServer(){
-        this.serverCreationStatus = "A new server was created with name: " + this.serverName;
-
     }
-    onUpdatedServerName(event: Event){
-        this.serverName = (<HTMLInputElement>event.target).value;
-
+    getColor(){
+        //Method to get the color of background
+        console.log(this.serverStatus);
+        console.log(this.serverStatus === 'Online' ? 'green': 'red');
+        return this.serverStatus === 'Online' ? 'green': 'red';
     }
 }
