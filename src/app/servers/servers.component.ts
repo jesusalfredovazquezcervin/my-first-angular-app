6187a180d21ff7beeffb1b661c7d4be2ace62735
JESUS ALFREDO VAZQUEZ CERVIN <jesus.alfredo.vazquez.cervin@gmail.com>
2022-09-29 12:10:22 -0500

    se modifican los archivos porque los habia escrito mal, tenia codigo del SERVER dentro del SERVERS

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,13 +3,37 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  
 })
 export class ServersComponent implements OnInit {
+  //serverID: number  = 10;
+  //serverStatus: string  = "Online";
+  allowNewServer = false;
+  serverCreationStatus = "No server created yet..."
+  serverName = '';
+  serverCreated = false;
+  servers = ['Test', 'Production'];
 
-  constructor() { }
+  constructor(){
+        setTimeout(() =>{
+            this.allowNewServer = true;
+        }, 3000);
+        
+    } 
+
+    onCreatedServer(){
+        this.serverCreationStatus = "A new server was created with name: " + this.serverName;
+        this.serverCreated = true;
+        this.servers.push(this.serverName);
+    }
+    onUpdatedServerName(event: Event){
+        this.serverName = (<HTMLInputElement>event.target).value;
+
+    }
+    
 
   ngOnInit(): void {
   }
+  
 
 }
